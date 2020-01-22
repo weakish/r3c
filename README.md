95fb5448aa7a82024fa84b8e14033ccc0abae96e
Jang Rush <weakish@gmail.com>
2020-01-23 01:20:58 +0800

    :new: -compress
    Disable compression by default.
    Add a new option `-compress` to enable it.

--- README.md
+++ README.md
@@ -14,12 +14,13 @@ will sync `dir1` and `dir2`. That's it.
 
 In some cases, if you do not care about unix features like permissions, links, etc, you can add an `--simple` option:
 
-    r3c --simple dir1 dir2
+    r3c -simple dir1 dir2
 
 For rsync users:
 
-- Without `--simple`, `-a -z --partial --delete`.
-- With `--simple`, same as above, but using `-r` instead of `-a`.
+- Without `-simple`, `-a --partial --delete`.
+- With `-simple`, same as above, but using `-r` instead of `-a`.
+- With `-compress`, passing the `-z` option.
 
 ## License
 
--- r3c.go
+++ r3c.go
@@ -9,18 +9,22 @@ import (
 
 func main() {
 	var simple *bool = flag.Bool("simple", false, "use -r instead of -a")
+	var compress *bool = flag.Bool("compress", false, "enable compression")
 	flag.Parse()
 
-	runRsync(*simple)
+	runRsync(*simple, *compress)
 }
 
-func runRsync(isSimple bool) {
+func runRsync(isSimple bool, enableCompress bool) {
 	var simple int = gosugar.Btoi(isSimple)
 
 	var opt string = [2]string{"-a", "-r"}[simple]
-	var args []string = []string{"rsync", opt, "-z", "--partial", "--delete"}
+	var args []string = []string{"rsync", opt, "--partial", "--delete"}
+	if enableCompress {
+		args = append(args, "-z");
+	}
 
-	if len(os.Args) != 3 + simple {
+	if len(os.Args) != 3 + simple + gosugar.Btoi(enableCompress) {
 		printUsage()
 	} else {
 		var sourceDir string = os.Args[1 + simple]
@@ -34,6 +38,6 @@ func runRsync(isSimple bool) {
 }
 
 func printUsage() {
-	os.Stderr.WriteString("Usage: r3c [--simple] directory_1 directory_2\n")
+	os.Stderr.WriteString("Usage: r3c [-simple] [-compress] directory_1 directory_2\n")
 	os.Exit(64)
 }
